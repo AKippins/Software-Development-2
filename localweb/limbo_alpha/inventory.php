0317888f4facb4f8f61847e72455202d4b2ad329
Aaron Kippins <AKippins@hotmail.com>
2013-11-14 11:33:44 -0500

    Altered inventory.php the query works!!!!!!! but theres a new error...

--- localweb/limbo_alpha/inventory.php
+++ localweb/limbo_alpha/inventory.php
@@ -15,7 +15,7 @@ require( 'limbo_alpha/connect_db.php' ) ;
 require( 'limbo_alpha/helpers.php' ) ;
 
 if ($_SERVER[ 'REQUEST_METHOD' ] == 'POST') {
-	$item_id = $_POST['item_id'] ;
+	#$item_id = $_POST['item_id'] ;
 
     $object = $_POST['object'] ;
 	
@@ -32,10 +32,7 @@ if ($_SERVER[ 'REQUEST_METHOD' ] == 'POST') {
 	$place_found = $_POST['place_found'] ;
 	
 
-    if(!valid_number($item_id)) {
-		echo '<p style="color:red;font-size:16px;">The item_id that you have entered is not a item_id!!!</p>';
-		}
-	else if (!valid_name($object)){
+    if (!valid_name($object)){
 		echo '<p style="color:red;font-size:16px;">That is not a valid Object!!!</p>';
 		}
 	else if (!valid_name($description)){
@@ -57,7 +54,7 @@ if ($_SERVER[ 'REQUEST_METHOD' ] == 'POST') {
 		echo '<p style="color:red;font-size:16px;">That is not a valid Place!!!</p>';
 		}
 	else
-	  $result = insert_record($dbc, $item_id, $object, $description, $size, $color, $weight, $date_found, $place_found) ;
+	  $result = insert_record($dbc, $object, $description, $size, $color, $weight, $date_found, $place_found) ;
 
       #echo "<p>Added " . $result . " new print(s) ". $name . " @ $" . $price . " .</p>" ;
     
@@ -75,7 +72,7 @@ if ($_SERVER[ 'REQUEST_METHOD' ] == 'POST') {
 
 
 # Show the records
-show_link_records($dbc);
+#show_link_records($dbc);
 
 # Close the connection
 mysqli_close( $dbc ) ;
@@ -83,7 +80,7 @@ mysqli_close( $dbc ) ;
 
 <!-- Display body section with sticky form. -->
 <form action="inventory.php" method="POST">
-<p>Item ID: <input type="int" name="item_id" value="<?php if (isset($_POST['item_id'])) echo $_POST['item_id']; ?>"> </p>
+<!--<p>Item ID: <input type="int" name="item_id" value="<?php #if (isset($_POST['item_id'])) echo $_POST['item_id']; ?>"> </p>-->
 <p>Object: <input type="text" name="object" value="<?php if (isset($_POST['object'])) echo $_POST['object']; ?>"></p>
 <p>Description: <input type="text" name="description" value="<?php if (isset($_POST['description'])) echo $_POST['description']; ?>"></p>
 <p>Size: <input type="text" name="size" value="<?php if (isset($_POST['size'])) echo $_POST['size']; ?>"></p>

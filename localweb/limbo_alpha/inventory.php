5c856c019d056ff4f58d0e69685fcda460aea2a1
Aaron Kippins <Aaron.Kippins1@marist.edu>
2013-11-14 23:30:34 -0500

    Got everything working, i'll do some tweaking and work on the losk found and quick links page. Don't forget to ask Zach to try to start the Admin Pages.

--- localweb/limbo_alpha/inventory.php
+++ localweb/limbo_alpha/inventory.php
@@ -80,44 +80,52 @@ mysqli_close( $dbc ) ;
 	<tr><td><p>Owner: </td> <td><input type="text" name="owner" value="<?php if (isset($_POST['owner'])) echo $_POST['owner']; ?>"></p></td></tr>
 	<tr><td><p>Finder: </td> <td><input type="text" name="finder" value="<?php if (isset($_POST['finder'])) echo $_POST['finder']; ?>"></p></td></tr>
 	<tr><td><p>Date Found: </td> <td><input type="date" name="date_found" value="<?php if (isset($_POST['date_found'])) echo $_POST['date_found']; ?>"></p></td></tr>
-	<tr><td><p>Place Found: </td> <td>
-									<?php 
-										# Connect to MySQL server and the database
-										require( 'limbo_alpha/connect_db.php' ) ;
-
-										$query = 'SELECT location_id, location_name FROM locations ORDER BY location_id DESC' ;
-
-										# Execute the query
-										$results = mysqli_query( $dbc , $query ) ;
-										check_results($results) ;
-
-										if($results){
-											echo '<select required=1 name="location_id">';
-											while ( $choice = mysql_fetch_array( $results, MYSQLI_ASSOC)) {
-												echo '<option value="' . $choice['location_id'] . '">' . $choice['location_name'] . '</option>';
-											}
-
-											echo '</select>';
-
-											# Free up the results in memory
-										  	mysqli_free_result( $results ) ;
-
-										}
-
-										# Checks the query results as a debugging aid
-										function check_results($results) {
-										  global $dbc;
-
-										  if($results != true)
-										    echo '<p>SQL ERROR = ' . mysqli_error( $dbc ) . '</p>'  ;
-									?>
+	<tr><td><p>Place Found: </td> <td><select required=1 name='location_id'>
+									  	<option value="">Select the location that the item was found</option>
+									  	<option value="1">Hancock Center</option>
+									  	<option value="2">Dyson Center</option>
+									  	<option value="3">Donnelly Hall</option>
+									  	<option value="4">Lowell Thomas</option>
+									  	<option value="5">Fontaine Hall</option>
+									  	<option value="6">Fontaine Annex</option>
+									  	<option value="7">Library</option>
+									  	<option value="8">Leo Hall</option>
+									  	<option value="9">Champagnat Hall</option>
+									  	<option value="10">Sheahan Hall</option>
+									  	<option value="11">Marian Hall</option>
+									  	<option value="12">Bryne House</option>
+									  	<option value="13">Our Lady Seat Of Wisdom Chapel</option>
+									  	<option value="14">Cornell Boathouse</option>
+									  	<option value="15">Fern Tor</option>
+									  	<option value="16">Foy Townhouses</option>
+									  	<option value="17">Fulton Street Townhouses</option>
+									  	<option value="18">New Fulton Townhouses</option>
+									  	<option value="19">Gartland Commons</option>
+									  	<option value="20">Greystone Hall</option>
+									  	<option value="21">Kieran Gatehouse</option>
+									  	<option value="22">Kirk House</option>
+									  	<option value="23">Longview Park</option>
+									  	<option value="24">Lower Townhouses</option>
+									  	<option value="25">Marist Boathouse</option>
+									  	<option value="26">McCann Center</option>
+									  	<option value="27">Midrise Hall</option>
+									  	<option value="28">New Townhouses</option>
+									  	<option value="29">St.Anns Hermitage</option>
+									  	<option value="30">St.Peters</option>
+									  	<option value="31">Steel Plant Art Studios</option>
+									  	<option value="32">Rotunda</option>
+									  	<option value="33">Tenney Stadium</option>
+									  	<option value="34">Tennis Pavilion</option>
+									  	<option value="35">Lower West Cedar Townhouses</option>
+									  	<option value="36">Upper West Cedar Townhouses</option>
+									  </select>
 									</p></td></tr>
 	<tr><td><p>Status:</td> <td><select required=1 name='status'>
 									<option value="">Select the status of the item</option>
 									<option value="'found'">Found</option>
 									<option value="'lost'">Lost</option>
 									<option value="'claimed'">Claimed</option>
-								</select>
+								</select></p></td></tr>
 </table>
 <p><input type="submit"></p>
 </form>
